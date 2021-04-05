c23763c091300de3aa5db37e4981cece88b1fd05
JermXT <jeremyzihanli@gmail.com>
2021-04-05 10:43:29 -0700

    works on gpu now, 99% test acc at epoch 22

--- main.py
+++ main.py
@@ -33,16 +33,18 @@ config = yaml.load(open(args.config, 'r'), Loader=yaml.FullLoader)
 
 
 def train(train_loader, model, criterion, optimizer, epoch):
+	model.train()
 	total_loss = 0
-	print("hi")
+	#print("hi")
 	for i, (input, target) in enumerate(train_loader):
 		#print(input.shape)
 		#print(target.shape)
 		optimizer.zero_grad()
-		print(input.shape)
-		predicted_label = model.forward(input)
-
-		loss = criterion(predicted_label, target)
+		#print(input.shape)
+		input = input.cuda()
+		predicted_label = model.forward(input).cuda()
+		target = target.cuda()
+		loss = criterion(predicted_label, target).cuda()
 		loss.backward()
 		optimizer.step()
 		total_loss += loss
@@ -50,12 +52,22 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
 
 def validate(val_loader, model, criterion):
-	total_loss = 0
-	for i, (input, target) in enumerate(val_loader):
-		predicted_label = model.forward(input)
-		loss = criterion(predicted_label, target)
-		total_loss += loss
-	return total_loss
+	model.eval()
+	with torch.no_grad():
+	    total_loss = 0
+	    total = 0
+	    accuracies = 0
+	    for i, (input, target) in enumerate(val_loader):
+		    #print(input.shape)
+		    #print(target.shape)
+		    input = input.cuda()
+		    target = target.cuda()
+		    predicted_label = model.forward(input).cuda()
+		    loss = criterion(predicted_label, target).cuda()
+		    total += 128
+		    accuracies += torch.sum(target == torch.argmax(predicted_label, dim=1))
+		    total_loss += loss
+	    return total_loss, accuracies/total
 
 def save_checkpoint(state, best_one, filename='rotationnetcheckpoint.pth.tar', filename2='rotationnetmodelbest.pth.tar'):
 	torch.save(state, filename)
@@ -64,11 +76,14 @@ def save_checkpoint(state, best_one, filename='rotationnetcheckpoint.pth.tar', f
 		shutil.copyfile(filename, filename2)
 
 def main():
+	print(torch.cuda.device_count(), "gpus available")
+	
 	# print(summary(model, (3, 32, 32)))
 	print("HEYO")
 
 	n_epochs = config["num_epochs"]
-	model = ResNet(0,0,0) #make the model with your paramters
+	print(n_epochs)
+	model = ResNet(0,0,0).cuda() #make the model with your paramters
     
 	criterion = nn.CrossEntropyLoss() #what is your loss function
     
@@ -92,7 +107,7 @@ def main():
 	print("hi")
 	all_batches = []
 	d_iter = iter(train_loader)
-	for i in range(40):
+	for i in range(45000//32):
 		image_stack = []
 		label_stack = []
 		for i in range(32):
@@ -108,6 +123,19 @@ def main():
 
 	val_dataset = './validation/' + '*' #how will you get your dataset
 	val_loader = CIFAR(val_dataset) # how will you use pytorch's function to build a dataloader
+	
+	val_batches = []
+	d_iter = iter(val_loader)
+	for i in range(4900//32):
+		image_stack = []
+		label_stack = []
+		for i in range(32):
+			temp = next(d_iter)
+			image_stack.append(temp[0])
+			label_stack.append(temp[1])
+		img = torch.cat(image_stack, axis=0)
+		lb = torch.cat(label_stack, axis=0)
+		val_batches.append((img, lb))
 
 	current_best_validation_loss = float("Infinity")
 
@@ -116,11 +144,12 @@ def main():
 	for epoch in range(n_epochs):
 		total_loss = train(all_batches, model, criterion, optimizer, epoch)
 		print("Epoch {0}: {1}".format(epoch, total_loss))
-		validation_loss = validate(val_loader, model, criterion)
-		print("\tTest Accuracy {0}".format(validation_loss))
-		if validation_loss < current_best_validation_loss:
+		validation_loss, accuracy = validate(val_batches, model, criterion)
+		print("Test Loss {0}".format(validation_loss))
+		print("Test Accuracy {0}".format(accuracy))
+		if accuracy < current_best_validation_loss:
 			save_checkpoint(model.state_dict(), True)
-			current_best_validation_loss = validation_loss
+			current_best_validation_loss = accuracy
 		else:
 			save_checkpoint(model.state_dict(), False)
 
